e1cf0ed55c9a99ba846e70b6af814fbadc049373
Oleg <yakushinoa@gmail.com>
2024-03-19 12:29:03 +0100

    Initial Commit

---
+++ POPatterns/POPatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  POPatternsApp.swift
+//  POPatterns
+//
+//  Created by Oleg Yakushin on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct POPatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
